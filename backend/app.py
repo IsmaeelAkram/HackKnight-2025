03ce3da39fe3dd16e22dacd98fd3c9dec0243df7
ismaeel <19865145+IsmaeelAkram@users.noreply.github.com>
2025-03-07 14:30:39 -0500

    fix

--- backend/app.py
+++ backend/app.py
@@ -1,54 +1,62 @@
-from dotenv import load_dotenv
-from flask import Flask, request, jsonify
 import os
+
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 from openai import OpenAI
-load_dotenv() #get api key from .env
-PERPLEXITY_API_KEY = os.environ.get('PERPLEXITY_API_KEY')
 
-@app.route("/market", methods=['POST'])
+load_dotenv()  # get api key from .env
+PERPLEXITY_API_KEY = os.environ.get("PERPLEXITY_API_KEY")
+client = OpenAI(api_key=PERPLEXITY_API_KEY, base_url="https://api.perplexity.ai")
+
+
+@app.route("/")
+def index():
+    return "HackKnight go!!! sike this is the backend that nobody cares about ^-^"
+
+
+@app.route("/market", methods=["POST"])
 def market_research():
     data = request.json
     idea = data.get("idea")
 
     if not idea:
-        return jsonify({"error": "no idea"}), 400
-    
+        return jsonify({"error": "Do you not have any ideas?"}), 400
+
+    print("Idea: ", idea)
+
     prompt = f"gimme market research for {idea}"
 
     messages = [
         {
             "role": "system",
-            "content": (
-                "You are an artificial intelligence assistant and you need to "
-                "engage in a helpful, detailed, polite conversation with a user."
-            ),
+            "content": "You are an artificial intelligence assistant and you need to "
+            "engage in a helpful, detailed, polite conversation with a user.",
         },
-        {   
+        {
             "role": "user",
-            "content": (prompt),
+            "content": prompt,
         },
     ]
 
-    client = OpenAI(api_key=PERPLEXITY_API_KEY, base_url="https://api.perplexity.ai")
-
-    #won't load until query completes
+    # won't load until query completes
     try:
+        print("Starting research...")
         response = client.chat.completions.create(
-            model="sonar-deep-research",
+            model="sonar",
             messages=messages,
         )
-        try:
-            return jsonify(response.choices[0].messages[0]), 200
-        except Exception as e:
-            return jsonify({"error" : str(e)}), 500
-    except Exception as e: 
-        return jsonify({"error" : str(e)}), 500
-    
-if(__name__ == "__main__"):
+        print("Research complete.")
+        print(response)
+        return jsonify(response.choices[0].message.content), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
     app.run()
 
 
-#{Market size estimation, market trend identification, SWOT analysis, competitive landscape analysis}
+# {Market size estimation, market trend identification, SWOT analysis, competitive landscape analysis}
